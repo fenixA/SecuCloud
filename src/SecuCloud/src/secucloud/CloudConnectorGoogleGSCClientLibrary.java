5d66d47804f5e40e9bba1e9aa9cc74e5a7b76e81
Fenix Assange <f.assange@googlemail.com>
2014-06-29 13:45:23 +0200

    File tests.

--- src/SecuCloud/src/secucloud/CloudConnectorGoogleGSCClientLibrary.java
+++ src/SecuCloud/src/secucloud/CloudConnectorGoogleGSCClientLibrary.java
@@ -1,18 +1,5 @@
 package secucloud;
 
-import com.google.appengine.tools.cloudstorage.GcsFileOptions;
-import com.google.appengine.tools.cloudstorage.GcsFilename;
-import com.google.appengine.tools.cloudstorage.GcsInputChannel;
-import com.google.appengine.tools.cloudstorage.GcsOutputChannel;
-import com.google.appengine.tools.cloudstorage.GcsService;
-import com.google.appengine.tools.cloudstorage.GcsServiceFactory;
-import com.google.appengine.tools.cloudstorage.RetryParams;
-
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.nio.ByteBuffer;
-
 public class CloudConnectorGoogleGSCClientLibrary extends CloudConnectorAbs {
 
 	public Boolean upload(String path) {
