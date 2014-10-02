5a3abc876137a74f69ed8c8facdbf9bbeab3f17b
fenix <f.assange@googlemail.com>
2014-10-02 12:34:45 +0200

    finished download

--- src/SecuCloud/src/control/util/CryptThreader.java
+++ src/SecuCloud/src/control/util/CryptThreader.java
@@ -1,6 +1,5 @@
 package control.util;
 
-
 import java.io.File;
 import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
@@ -21,6 +20,7 @@ public class CryptThreader implements Runnable {
 
 	private InformationContainer informationContainer;
 	private command cmd;
+	private Thread t;
 
 	public CryptThreader(command cmd, InformationContainer informationContainer) {
 		this.informationContainer = informationContainer;
@@ -31,21 +31,45 @@ public class CryptThreader implements Runnable {
 	public void run() {
 		switch (cmd) {
 		case encryptFile:
-			File selectedFile = new File(informationContainer.getLocalPLainLocation());
+			File selectedFile = new File(
+					informationContainer.getLocalPLainLocation());
 			try {
 				CryptToolbox.threadEncryptFileAesCTR(selectedFile,
-						informationContainer.getLocalEncryptedLocation(), informationContainer.getKey());
+						informationContainer.getLocalEncryptedLocation(),
+						informationContainer.getKey());
 			} catch (InvalidKeyException | NoSuchAlgorithmException
 					| NoSuchProviderException | NoSuchPaddingException
 					| InvalidAlgorithmParameterException | IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			Thread t = new Thread(new CloudConnectThreader(CloudConnectThreader.command.upload, informationContainer));
-			t.start();
-			Main.getInstance().threadVector.add(t);
+			this.t = new Thread(new CloudConnectThreader(
+					CloudConnectThreader.command.upload, informationContainer));
+			this.t.start();
+			Main.getInstance().threadVector.add(this.t);
 		case decryptFile:
-			break;
+			this.t = new Thread(
+					new CloudConnectThreader(
+							CloudConnectThreader.command.download,
+							informationContainer));
+			this.t.start();
+			try {
+				this.t.join();
+				File f = new File(Main.getInstance().getUSER_TEMP_DIR() + "/"
+						+ informationContainer.getName());
+				CryptToolbox.threadDecryptFileAesCTR(f,
+						Main.getInstance().getUSER_DOWNLOAD_DIR() + "/"
+								+ informationContainer.getName(),
+						informationContainer.getKey());
+				f.delete();
+			} catch (InterruptedException | InvalidKeyException
+					| NoSuchAlgorithmException | NoSuchProviderException
+					| NoSuchPaddingException
+					| InvalidAlgorithmParameterException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
 		}
 	}
 }
